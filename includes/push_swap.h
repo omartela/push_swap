1e0c9f566c59dc8e4580d9474a63b6d9123c6cce
omartela <oskari@gmail.com>
2024-07-15 09:29:04 +0300

    Updates header file

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: omartela <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 15:49:54 by omartela          #+#    #+#             */
-/*   Updated: 2024/07/08 15:54:35 by omartela         ###   ########.fr       */
+/*   Updated: 2024/07/12 17:59:42 by omartela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PUSH_SWAP_H
@@ -43,8 +43,6 @@ void	rotate(t_stack **stack);
 void	reverse_rotate(t_stack **stack);
 void	push(t_stack **stack, t_stack *stack_node);
 t_stack	*pop(t_stack **stack);
-int		is_empty(t_stack **stack);
-int		is_full(t_stack **stack);
 int		is_sorted(t_stack **stack);
 int		sort_three(t_stack **stack);
 t_node	*find_min(t_stack **stack);
@@ -56,7 +54,6 @@ void	pa(t_stack **a, t_stack **b);
 void	pb(t_stack **a, t_stack **b);
 void	init_stack(t_stack **stack);
 int		init_stack_a(t_stack **stack, char **str);
-void	init_stack_b(t_stack **a, t_stack **b);
 void	sort_stacks(t_stack **a, t_stack **b);
 void	free_stack(t_stack **stack);
 void	ra(t_stack **a);
@@ -66,4 +63,12 @@ void	rra(t_stack **a);
 void	rrb(t_stack **b);
 void	rrr(t_stack **a, t_stack **b);
 t_stack	*last_stack(t_stack *stack);
+void	set_median(t_stack **a);
+void	set_target_a(t_stack **a, t_stack **b);
+void	set_target_b(t_stack **a, t_stack **b);
+void	calculate_push_cost_a(t_stack **a);
+void	set_cheapest(t_stack **a);
+void	prepare_for_push(t_stack **stack, t_node *cheapest, char indicator);
+void	move_node_a_to_b(t_stack **a, t_stack **b);
+
 #endif
